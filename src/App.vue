7509a7b66b6782fa187ba85b4a7159b5922b6a44
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2022-11-18 01:11:46 +0100

    Restart

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <!--<router-link to="/home">Home</router-link>-->
-      <HelloWorld></HelloWorld>
-    </div>
-    <router-view />
+  <div class="hidden">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <HelloWorld msg="Vite + Vue" />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+<style scoped>
+import .logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
